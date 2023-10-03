c4707d9908f4affbc07eecd7d43c6bfd94678705
ambiknds <53797801+ambiknds@users.noreply.github.com>
2023-10-03 19:15:47 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <div className='content'>
+        <Home />
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <h1>Simple Blog</h1>
+      <div className='links'>
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </div>
+  )
+}
